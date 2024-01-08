b031cf7064db59db4bfcd7db009a9e6698d45fcd
Theodora <110998333+Lemma-cloud7@users.noreply.github.com>
2024-01-08 10:58:22 +0000

    Update variables.tf

--- aks-terraform/variables.tf
+++ aks-terraform/variables.tf
@@ -8,15 +8,3 @@ variable "service_principal_secret" {
   type        = string
 }
 
-
-
-##################
-# variable "service_principal_client_id" {
-#   description = "The Client ID for the service principal."
-#   type        = string
-# }
-
-# variable "service_principal_secret" {
-#   description = "The Client Secret for the service principal."
-#   type        = string
-# }
