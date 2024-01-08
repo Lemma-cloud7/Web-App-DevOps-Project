c5f1aded30dd34ba1622dc3251aef8e2be25b864
Theodora <110998333+Lemma-cloud7@users.noreply.github.com>
2024-01-08 11:11:22 +0000

    Update variables.tf

--- aks-terraform/variables.tf
+++ aks-terraform/variables.tf
@@ -1,22 +1,9 @@
-# Variables for the Networking Module
-
-# Variable for Azure Resource Group Name
-variable "resource_group_name" {
-  description = "The name of the Azure Resource Group where networking resources will be deployed."
+variable "service_principal_client_id" {
+  description = "The Client ID for the service principal."
   type        = string
-  default     = "myResourceGroup"  # Replace with your desired default value
 }
 
-# Variable for Azure Region Location
-variable "location" {
-  description = "The Azure region where the networking resources will be deployed."
+variable "service_principal_secret" {
+  description = "The Client Secret for the service principal."
   type        = string
-  default     = "East US"  # Replace with your desired default value
-}
-
-# Variable for Virtual Network Address Space
-variable "vnet_address_space" {
-  description = "The address space for the Virtual Network (VNet)."
-  type        = list(string)
-  default     = ["10.0.0.0/16"]  # Replace with your desired default value
 }
