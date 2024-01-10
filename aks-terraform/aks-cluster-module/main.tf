a814cbfba84b822301a59d020c0733767c51058a
Theodora <110998333+Lemma-cloud7@users.noreply.github.com>
2024-01-10 07:42:22 +0000

    Update main.tf

--- aks-terraform/aks-cluster-module/main.tf
+++ aks-terraform/aks-cluster-module/main.tf
@@ -6,34 +6,17 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   dns_prefix          = var.dns_prefix
   kubernetes_version  = var.kubernetes_version
 
-#   default_node_pool {
-#     name                = "default"
-#     node_count          = 3  
-#     vm_size             = "Standard_DS2_v2"  
-#     vnet_subnet_id      = var.worker_node_subnet_id
-#   }
-
   default_node_pool {
-    name                = "default"
-    node_count          = 1  # Minimize the number of nodes
-    vm_size             = "Standard_B2s"  
-    vnet_subnet_id      = var.worker_node_subnet_id
-    enable_auto_scaling = true  # Enable auto-scaling
-    min_count           = 1
-    max_count           = 2
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+    enable_auto_scaling = true
+    min_count = 1
+    max_count = 3
   }
+
   service_principal {
     client_id     = var.service_principal_client_id
-    client_secret = var.service_principal_secret
-  }
-
-  network_profile {
-    network_plugin     = "azure"
-    network_policy     = "calico"
-    dns_service_ip     = "10.1.0.10"  # 
-    #docker_bridge_cidr = "172.17.0.1/16"  # 
-    service_cidr       = "10.1.0.0/16"  #
+    client_secret = var.service_principal_client_secret
   }
 }
-
-
